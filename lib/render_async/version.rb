f1ebf60432d2350f6af4f31a3076c452d71dc27f
Kasper Grubbe <kawsper@gmail.com>
2013-08-24 02:07:14 +0200

    It is now safe to call the method render_async instead of render_asynk (Billetto namespace issue)

--- lib/render_async/version.rb
+++ lib/render_async/version.rb
@@ -1,3 +1,3 @@
 module RenderAsync
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
--- lib/render_async/view_helpers.rb
+++ lib/render_async/view_helpers.rb
@@ -2,7 +2,7 @@ require 'securerandom'
 
 module RenderAsync
   module ViewHelpers
-    def render_asynk(path)
+    def render_async(path)
       container_name = "render_async_#{SecureRandom.hex(5)}#{Time.now.to_i}"
 
       render 'render_async/render_async', {:container_name => container_name, :path => path}
