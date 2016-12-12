535f0dae6b755b2b52025c58c3d2d7fdb2724b43
nikolalsvk <nikolaseap@gmail.com>
2016-12-12 16:04:18 +0100

    Typo

--- lib/render_async/railtie.rb
+++ lib/render_async/railtie.rb
@@ -2,7 +2,7 @@ require 'render_async/view_helper'
 
 module RenderAsync
   class Railtie < Rails::Railtie
-    initializer "render_async.view_helpers" do
+    initializer "render_async.view_helper" do
       ActionView::Base.send :include, ViewHelper
     end
   end
