63c4d4fecb39dda41b462474c4528bd644fff6a0
Josh Arnold <josharnold.sv@gmail.com>
2017-06-08 14:57:35 -0400

    Create view_helper.rb
    Potentially take advantage of rails fragment/view caching.
    
    Your view partial would need to use some established cache key method. I just used "views/#{path}_cached" as a quick example.
    
    In your view:
    `<% cache "#{path}_cached" do %>
       '<div>Some code</div>'
    <% end %>`

--- lib/render_async/view_helper.rb
+++ lib/render_async/view_helper.rb
@@ -4,6 +4,9 @@ module RenderAsync
   module ViewHelper
 
     def render_async(path)
+      cached_view = Rails.cache.read("views/#{path}_cached")
+      render cached_view and return if cached_view.present?
+      
       container_name = "render_async_#{SecureRandom.hex(5)}#{Time.now.to_i}"
 
       render "render_async/render_async", :container_name => container_name,
