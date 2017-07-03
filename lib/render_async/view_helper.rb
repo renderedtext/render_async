f17afd300890374ad4d50fb7913f2694fc8651f9
colinxfleming <c3flemin@gmail.com>
2017-07-03 17:18:42 -0400

    make it take html options, please

--- lib/render_async/view_helper.rb
+++ lib/render_async/view_helper.rb
@@ -3,11 +3,12 @@ require 'securerandom'
 module RenderAsync
   module ViewHelper
 
-    def render_async(path)
+    def render_async(path, html_options = {})
       container_name = "render_async_#{SecureRandom.hex(5)}#{Time.now.to_i}"
 
       render "render_async/render_async", :container_name => container_name,
-                                          :path => path
+                                          :path => path,
+                                          :html_options => html_options
     end
 
   end
