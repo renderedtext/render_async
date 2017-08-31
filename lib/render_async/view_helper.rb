5bfb919d3b677c35cd31e262ddd775ff867211e0
Josh Arnold <jjarnold@brotherhoodmutual.com>
2017-08-31 17:43:04 -0400

    Add view caching support

--- lib/render_async/view_helper.rb
+++ lib/render_async/view_helper.rb
@@ -3,6 +3,16 @@ require 'securerandom'
 module RenderAsync
   module ViewHelper
 
+    def render_async_cache_key(path)
+      "render_async_#{path}"
+    end
+
+    def render_async_cache(path, html_options = {})
+      cached_view = Rails.cache.read("views/#{render_async_cache_key(path)}")
+      render cached_view and return if cached_view.present?
+      render_async(path, html_options)
+    end
+
     def render_async(path, html_options = {})
       container_name = "render_async_#{SecureRandom.hex(5)}#{Time.now.to_i}"
 
