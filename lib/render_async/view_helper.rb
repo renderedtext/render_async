e5767ce0ae65d87febd862040c995b5164f14727
Rene Klacan <rene.klacan@gmail.com>
2018-03-25 03:42:40 +0200

    Configurable AJAX method, headers and body

--- lib/render_async/view_helper.rb
+++ lib/render_async/view_helper.rb
@@ -18,17 +18,23 @@ module RenderAsync
     end
 
     def render_async(path, options = {}, &placeholder)
-      html_element_name = options.delete(:html_element_name) || "div"
+      html_element_name = options.delete(:html_element_name) || 'div'
       container_id = "render_async_#{SecureRandom.hex(5)}#{Time.now.to_i}"
       event_name = options.delete(:event_name)
       placeholder = capture(&placeholder) if block_given?
+      method = options.delete(:method) || 'GET'
+      data = options.delete(:data)
+      headers = options.delete(:headers) || {}
 
       render 'render_async/render_async', html_element_name: html_element_name,
                                           container_id: container_id,
                                           path: path,
                                           html_options: options,
                                           event_name: event_name,
-                                          placeholder: placeholder
+                                          placeholder: placeholder,
+                                          method: method,
+                                          data: data,
+                                          headers: headers
     end
 
   end
