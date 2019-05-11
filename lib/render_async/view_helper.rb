d1a38636b195fa46a8a10f028324a1c53f9d5069
Nikola Đuza <nikolaseap@gmail.com>
2019-05-11 02:45:27 +0200

    Add more polling options and refactor options fetching

--- lib/render_async/view_helper.rb
+++ lib/render_async/view_helper.rb
@@ -18,36 +18,46 @@ module RenderAsync
     end
 
     def render_async(path, options = {}, &placeholder)
-      html_element_name = options.delete(:html_element_name) || 'div'
-      container_id = options.delete(:container_id) || generate_container_id
-      container_class = options.delete(:container_class)
       event_name = options.delete(:event_name)
       placeholder = capture(&placeholder) if block_given?
-      method = options.delete(:method) || 'GET'
-      data = options.delete(:data)
-      headers = options.delete(:headers) || {}
-      error_message = options.delete(:error_message)
-      error_event_name = options.delete(:error_event_name)
       retry_count = options.delete(:retry_count) || 0
-      interval = options.delete(:interval)
 
-      render 'render_async/render_async', html_element_name: html_element_name,
-                                          container_id: container_id,
-                                          container_class: container_class,
+      render 'render_async/render_async', **container_element_options(options),
                                           path: path,
                                           html_options: options,
                                           event_name: event_name,
                                           placeholder: placeholder,
-                                          method: method,
-                                          data: data,
-                                          headers: headers,
-                                          error_message: error_message,
-                                          error_event_name: error_event_name,
+                                          **request_options(options),
+                                          **error_handling_options(options),
                                           retry_count: retry_count,
-                                          interval: interval
+                                          **polling_options(options)
     end
 
     private
+
+    def container_element_options(options)
+      { html_element_name: options[:html_element_name] || 'div',
+        container_id: options[:container_id] || generate_container_id,
+        container_class: options[:container_class] }
+    end
+
+    def request_options(options)
+      { method: options[:method] || 'GET',
+        data: options[:data],
+        headers: options[:headers] || {} }
+    end
+
+    def error_handling_options(options)
+      { error_message: options[:error_message],
+        error_event_name: options[:error_event_name] }
+    end
+
+    def polling_options(options)
+      { interval: options[:interval], 
+        start_poll_on_page_load: options[:start_poll_on_page_load],
+        start_poll_event_name: options[:start_poll_event_name],
+        stop_poll_event_name: options[:stop_poll_event_name] }
+    end
 
     def generate_container_id
       "render_async_#{SecureRandom.hex(5)}#{Time.now.to_i}"
